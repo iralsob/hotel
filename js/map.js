b78f6f402d28dd6e5ae563d26fdf9e78a7d936b5
Ira <iralsob@gmail.com>
2015-01-19 14:38:23 +0300

    first

--- js/map.js
+++ js/map.js
@@ -18,25 +18,25 @@ function init () {
 	var myPlacemark1 = new ymaps.Placemark([55.74, 37.56], {}, {
 		iconLayout: 'default#image',
 		iconImageHref: 'http://iralsob.github.io/hotel/img/mark.png',
-		iconImageSize: [50, 65],
+		iconImageSize: [31, 42],
 		iconImageOffset: [-3, -42]
 	});
 	var myPlacemark2 = new ymaps.Placemark([55.78, 37.56], {}, {
 		iconLayout: 'default#image',
 		iconImageHref: 'http://iralsob.github.io/hotel/img/mark.png',
-		iconImageSize: [50, 65],
+		iconImageSize: [31, 42],
 		iconImageOffset: [-3, -42]
 	});
 	var myPlacemark3 = new ymaps.Placemark([55.78, 37.54], {}, {
 		iconLayout: 'default#image',
 		iconImageHref: 'http://iralsob.github.io/hotel/img/mark.png',
-		iconImageSize: [50, 65],
+		iconImageSize: [31, 42],
 		iconImageOffset: [-3, -42]
 	});
 	var myPlacemark4 = new ymaps.Placemark([55.78, 37.56], {}, {
 		iconLayout: 'default#image',
 		iconImageHref: 'http://iralsob.github.io/hotel/img/mark.png',
-		iconImageSize: [50, 65],
+		iconImageSize: [31, 42],
 		iconImageOffset: [-3, -42]
 	});
 
